2f6ce1499ecc9bc75f0eac652b600720caa9acd7
aydeeen <aydinholjan@gmail.com>
2019-10-06 19:45:27 +0200

    Updates

---
+++ precache-manifest.23ecfa935110d982a3d7d2fa6ecd00ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea2a76070ed2e6aec16e1a1299966bf",
+    "url": "/Logger/index.html"
+  },
+  {
+    "revision": "8767e12935f98ade744c",
+    "url": "/Logger/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "8767e12935f98ade744c",
+    "url": "/Logger/static/js/2.bdda48cc.chunk.js"
+  },
+  {
+    "revision": "3b7f6d9ab52f7718dd1b",
+    "url": "/Logger/static/js/main.dc0ca7b5.chunk.js"
+  },
+  {
+    "revision": "86c07dc5e6e6cafcb64e",
+    "url": "/Logger/static/js/runtime-main.4b085967.js"
+  }
+]);
